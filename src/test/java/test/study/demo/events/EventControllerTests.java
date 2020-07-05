f2f362554f4886529906b40d0683a547e9228d57
HongSooAh37 <hsa3737@naver.com>
2020-07-05 16:35:14 +0900

    Ctreate first Test:createEvent

---
+++ src/test/java/test/study/demo/events/EventControllerTests.java
@@ -0,0 +1,55 @@
+package test.study.demo.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void createEvent() throws Exception {
+
+        Event event = Event.builder()
+                .name("Srping")
+                .description("Spring Api to studying")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,6,5,16,24,1))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,6,6,16,25,1))
+                .beginEventDateTime(LocalDateTime.of(2020,7,1,9,1,3))
+                .endEventDateTime(LocalDateTime.of(2020,7,2,17,59,59))
+                .basePrice(100)
+                .maxPrice(200)
+                .location("강남역 어딘가")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(event))
+                .accept(MediaTypes.HAL_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                ;
+
+    }
+}
