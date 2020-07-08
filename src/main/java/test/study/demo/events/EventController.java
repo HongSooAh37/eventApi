e95d422472b8d14476eb77369c83260ba386f56a
HongSooAh37 <hsa3737@naver.com>
2020-07-08 23:47:17 +0900

    apply for HATEOS

--- src/main/java/test/study/demo/events/EventController.java
+++ src/main/java/test/study/demo/events/EventController.java
@@ -3,6 +3,7 @@ package test.study.demo.events;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
@@ -49,8 +50,16 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(event);
+
+        var selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
+
+        EventResource eventResource = new EventResource(event);
+        //eventResource.add(selfLinkBuilder.withSelfRel());
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+
+        return ResponseEntity.created(createdUri).body(eventResource);
 
     }
 }
---
+++ src/main/java/test/study/demo/events/EventResource.java
@@ -0,0 +1,26 @@
+package test.study.demo.events;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+public class EventResource extends EntityModel<Event> {
+
+    public EventResource(Event event, Link... links){
+        super(event, links);
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+
+    }
+
+/*    @JsonUnwrapped
+    private Event event;
+
+    public EventResource(Event event) {
+        this.event = event;
+    }
+
+    public Event getEvent() {
+        return event;
+    }*/
+}
