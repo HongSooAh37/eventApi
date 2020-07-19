b8dc15150dceb7ecb13a58994c486c6c3b74a971
HongSooAh37 <hsa3737@naver.com>
2020-07-19 13:21:57 +0900

    TEST OF Account Exception

--- src/test/java/test/study/demo/accounts/AccountServiceTest.java
+++ src/test/java/test/study/demo/accounts/AccountServiceTest.java
@@ -1,17 +1,22 @@
 package test.study.demo.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,6 +28,9 @@ public class AccountServiceTest {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void findByUsername(){
@@ -40,6 +48,30 @@ public class AccountServiceTest {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
         assertThat(userDetails.getPassword()).isEqualTo(password);
+
+    }
+
+   /* 1. @Test(expected = UsernameNotFoundException.class)*/
+    @Test
+    public void findByUsernameFail(){
+
+      /* 1.  String username = "randome@mail.com";
+             accountService.loadUserByUsername(username);   */
+
+      /*  2.  String username = "randome@mail.com";
+
+        try {
+            accountService.loadUserByUsername(username);
+            fail("username No Auth");
+        }catch (UsernameNotFoundException e){
+            assertThat(e.getMessage()).containsSequence(username);
+        }*/
+
+        String username = "randome@mail.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        accountService.loadUserByUsername(username);
 
     }
 
